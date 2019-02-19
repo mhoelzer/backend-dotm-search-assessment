f12b7edbf9e4a93efff43800864084dd223081d6
Michelle Hoelzer <hoelzermm@gmail.com>
2019-02-19 18:13:20 -0500

    have some stuff working

--- dotm_search.py
+++ dotm_search.py
@@ -3,14 +3,45 @@
 
 """
 Given a directory path, search all files in the path for a given text string
-within the 'word/document.xml' section of a MSWord .dotm file.
+within the "word/document.xml" section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "mhoelzer"
 
 
-def main():
-    raise NotImplementedError("Your awesome code begins here!")
+import argparse
+import os
+import zipfile
+
+# open dotm fileand earch text ; $; dotmfiles; rerad binary
 
 
-if __name__ == '__main__':
-    main()
+def main(directory, text_to_search):
+    # os listdir to get outer then go in for .m
+    # dotm is a zip
+    # ptyhon zip file tutorrial
+    # if $ in file...
+    dir_path = os.listdir(directory)
+    files_searched = 0
+    files_matched = 0
+    with zipfile.ZipFile(dir_path, "r") as zipped_file:
+        if dir_path.endswith(".dotm"):
+            with zipped_file.open("word/document.xml", "r") as opened_file:
+                for file in dir_path:
+                    print(file)
+    print("Searching directory {} for text \"{}\" ...".format(
+        dir_path, text_to_search))
+    print("Files searched: {}".format(files_searched))
+    print("Files matched: {}".format(files_matched))
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Process files to find dotm")
+    parser.add_argument("--dir", help="directory for searching")
+    parser.add_argument("text_to_search", help="text for searching")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+    main(args.directory, args.text_to_search)
